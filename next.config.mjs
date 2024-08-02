03a78580c8c763347247d0a3ce7818d79823cebd
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-02 23:56:14 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+// next.config.js
+
+
 const nextConfig = {
-  reactStrictMode: true,
+   images:{
+    remotePatterns:{
+      protocol:"https",
+      hostname:"**"
+    }
+   }
 };
 
 export default nextConfig;
