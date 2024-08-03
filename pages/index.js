88a44da56219b0abd8162b9baa9965851515ffa1
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-03 11:03:24 +0530

    changed

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import { useRouter } from "next/router"
 function Main()
 {
+       const route=useRouter();
+         const viewProducts=()=>{
+              route.push("/products")
+         }
+
     return(
         <>
            <h1>Main page</h1>
+             <button className="bg-blue-600 rounded p-2" onClick={viewProducts}>ViewProducts</button>
         </>
     )
 }
