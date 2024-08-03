83bb11321fc181242f9b64b3db3f41ed4231d29b
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-03 11:55:27 +0530

    done

--- pages/products/index.js
+++ pages/products/index.js
@@ -34,13 +34,19 @@ export default function Home() {
     <div className="flex flex-wrap gap-14 p-4" >
         
       {data.map((res) => (
+        <div>
         <div key={res.product_id} className="bg-slate-200 p-4 text-center cursor-pointer rounded-lg object-contain h-auto hover:bg-slate-600 hover:text-white" onClick={()=>showProduct(res.product_id)}>
          
           <Image src={res.image_link} alt={res.description} width={500} height={300}/>
             <p>Name:{res.name},name</p>
           <p>{res.price}</p>
-            <button className="bg-yellow-400 rounded p-2">AddToCart</button>
+           
         </div>
+        <div>
+        <button className="bg-yellow-400 rounded p-2 cursor-pointer">AddToCart</button>
+        </div>
+        </div>
+        
       ))}
     </div>
   );
