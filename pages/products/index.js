cd18289eb5bbe62ef2ae1ef590c8db7c5d8648f6
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-07 00:17:54 +0530

    modified

--- pages/products/index.js
+++ pages/products/index.js
@@ -8,24 +8,7 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
     const router=useRouter()
-  useEffect(() => {
-    async function fetchProducts() {
-      try {
-        const response = await fetch(`api/server`);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchProducts();
-  }, []);
+  
   const addToCart = async (product) => {
     try {
       const res = await fetch('/api/server', {
