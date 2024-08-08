561d4a78655a8156430c71b83dc54addf06dcbf6
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2024-08-08 22:36:46 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 
 
 const nextConfig = {
- reactStrictMode: false,
+ reactStrictMode: true,
    images:{
     remotePatterns:[
       {
@@ -13,6 +13,27 @@ const nextConfig = {
     ],
     domains: ['cdn-tp2.mozu.com']
    },
+   async headers(){
+       return[
+        {
+          source:'api/:path*',
+        
+        headers:[
+          {key:'Access-Control-Allow-Credentials',value:'true'},
+          {key:'Access-Control-Allow-Origin',value:'*'},
+          {key:'Access-Control-Allow-Methods',
+            value:'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key:'Access-Control-Allow-Headers',
+            value:'X-CSRF-Token,X-Requested-With,Accept,Accept-version,Content-Length,Content-MD5,Content-Type,Date'
+          },
+        ]
+          },
+        ]
+       
+      }
+   
 };
 
 export default nextConfig;
